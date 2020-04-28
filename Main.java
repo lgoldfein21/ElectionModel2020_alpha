2d0907ef5c9a2f6e3cee9a0e0cf170afd533a69c
lgoldfein21 <lgoldfein21@cgps.org>
2020-04-28 19:27:37 +0000

    Formatted code

--- Main.java
+++ Main.java
@@ -1,159 +1,157 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-       
-        
+public class Main {
+    public static void main(String[] args) {
+
+
         State NY = new State("New York", true, true, true, 58.8, 37.5);
         System.out.println(NY);
-        
+
         State NJ = new State("New Jersey", true, true, true, 55.0, 41.8);
         System.out.println(NJ);
-        
+
         State CA = new State("California", true, true, true, 61.5, 33.2);
         System.out.println(CA);
-  
+
         State AZ = new State("Arizona", false, false, false, 48.6, 44.2);
         System.out.println(AZ);
-        
+
         State AL = new State("Alabama", false, false, false, 34.6, 62.9);
         System.out.println(AL);
-    
+
         State TX = new State("Texas", false, false, false, 45.3, 47.5);
         System.out.println(TX);
-    
+
         State FL = new State("Florida", false, true, true, 46.5, 43.3);
         System.out.println(FL);
-        
+
         State PA = new State("Pennsylvania", false, true, true, 48.0, 41.3);
         System.out.println(PA);
-        
+
         State NC = new State("North Carolina", false, false, true, 46.0, 47.3);
         System.out.println(NC);
-        
+
         State SC = new State("South Carolina", false, false, false, 40.8, 54.9);
         System.out.println(SC);
-        
+
         State MN = new State("Minnesota", true, true, true, 46.9, 45.4);
         System.out.println(MN);
-        
+
         State WI = new State("Wisconsin", false, true, true, 46.7, 44.0);
         System.out.println(WI);
-        
+
         State MI = new State("Michigan", false, true, true, 46.5, 41.0);
         System.out.println(MI);
-        
+
         State VA = new State("Virginia", true, true, true, 48.3, 41.0);
         System.out.println(VA);
-        
+
         State NM = new State("New Mexico", true, true, true, 48.3, 40.0);
         System.out.println(NM);
-        
+
         State CO = new State("Colorado", true, true, true, 47.2, 44.4);
         System.out.println(CO);
-        
+
         State UT = new State("Utah", false, false, false, 27.8, 45.9);
         System.out.println(UT);
-        
+
         State OH = new State("Ohio", false, true, true, 51.3, 44.7);
         System.out.println(OH);
-        
+
         State IN = new State("Indiana", false, false, true, 37.9, 57.2);
         System.out.println(IN);
-        
+
         State IL = new State("Illinois", true, true, true, 55.4, 39.4);
         System.out.println(IL);
-        
+
         State IA = new State("Iowa", false, true, true, 44.0, 48.8);
         System.out.println(IA);
-        
+
         State GA = new State("Georgia", false, false, false, 43.5, 51.0);
         System.out.println(GA);
-        
+
         State ND = new State("North Dakota", false, false, false, 27.8, 64.1);
         System.out.println(ND);
-        
+
         State SD = new State("South Dakota", false, false, false, 31.7, 61.5);
         System.out.println(SD);
-        
+
         State ID = new State("Idaho", false, false, false, 27.6, 59.2);
         System.out.println(ID);
-        
+
         State MS = new State("Mississippi", false, false, false, 39.7, 58.3);
         System.out.println(MS);
-        
+
         State MO = new State("Missouri", false, false, false, 38.0, 57.1);
-        System.out.println(MO); 
-        
+        System.out.println(MO);
+
         State LA = new State("Louisiana", false, false, false, 38.4, 58.1);
-        System.out.println(LA); 
-        
+        System.out.println(LA);
+
         State MT = new State("Montana", false, false, false, 36.0, 56.5);
-        System.out.println(MT); 
-        
+        System.out.println(MT);
+
         State NE = new State("Nebraska", false, false, false, 34.0, 60.3);
-        System.out.println(NE); 
-        
+        System.out.println(NE);
+
         State AK = new State("Alaska", false, false, false, 37.7, 52.9);
-        System.out.println(AK); 
-        
+        System.out.println(AK);
+
         State KS = new State("Kansas", false, false, false, 36.2, 57.2);
-        System.out.println(KS); 
-        
+        System.out.println(KS);
+
         State AR = new State("Arkansas", false, false, false, 33.8, 60.4);
-        System.out.println(AR); 
-        
+        System.out.println(AR);
+
         State TN = new State("Tennessee", false, false, false, 34.9, 61.1);
-        System.out.println(TN); 
-        
+        System.out.println(TN);
+
         State KY = new State("Kentucky", false, false, false, 32.7, 62.5);
-        System.out.println(KY); 
-        
+        System.out.println(KY);
+
         State OK = new State("Oklahoma", false, false, false, 28.9, 65.3);
-        System.out.println(OK); 
-        
+        System.out.println(OK);
+
         State WV = new State("West Virginia", false, false, false, 26.5, 68.7);
-        System.out.println(WV); 
-        
+        System.out.println(WV);
+
         State WY = new State("Wyoming", false, false, false, 22.5, 70.1);
-        System.out.println(WY); 
-        
+        System.out.println(WY);
+
         State NV = new State("Nevada", true, true, true, 48.3, 44.7);
-        System.out.println(NV); 
-        
+        System.out.println(NV);
+
         State OR = new State("Oregon", true, true, true, 51.7, 41.1);
         System.out.println(OR);
-        
+
         State WA = new State("Washington", true, true, true, 54.4, 38.2);
         System.out.println(WA);
-        
+
         State ME = new State("Maine", true, true, true, 53.3, 43.3);
         System.out.println(ME);
-        
+
         State MD = new State("Maryland", true, true, true, 60.5, 35.3);
         System.out.println(MD);
-        
+
         State DE = new State("Delaware", true, true, true, 53.4, 41.9);
         System.out.println(DE);
-        
+
         State CT = new State("Connecticut", true, true, true, 54.5, 41.2);
         System.out.println(CT);
-        
+
         State MA = new State("Massachusetts", true, true, true, 60.8, 33.5);
         System.out.println(MA);
-        
+
         State VT = new State("Vermont", true, true, true, 61.1, 32.6);
         System.out.println(VT);
-        
+
         State NH = new State("New Hampshire", true, true, true, 49.5, 45.0);
         System.out.println(NH);
-        
+
         State RI = new State("Rhode Island", true, true, true, 55.4, 39.8);
         System.out.println(RI);
-        
+
         State HI = new State("Hawaii", true, true, true, 62.3, 30.1);
         System.out.println(HI);
-        
+
         State DC = new State("DC", true, true, true, 92.8, 4.1);
         System.out.println(DC);
     }
